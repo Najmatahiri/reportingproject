2d3a84f039493e24d1f29e93879b6072c216597e
Laminou Abdoul Bassit <abdoulbassitlamine123@gmail.com>
2024-05-10 22:12:46 +0100

    initial commit

--- reporting/ressources.py
+++ reporting/ressources.py
@@ -6,7 +6,7 @@ from .models import MachineVM
 class MachineVMResource(resources.ModelResource):
     class Meta:
         model = MachineVM
-        import_id_fields = ("ip", "date_import")
+        import_id_fields = ("ip",)
         skip_unchanged = True
         report_skipped = False
         # exclude = ('id',"date_import", "slug")
